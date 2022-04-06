b5181f7786c00e71084141ce6953e34ea281f4fa
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-04-06 16:05:57 +0200

    Har gjort en grund i main.. Ska kolla vad vi kan förenkla och ändra imorgon

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import { Home } from "./pages/home.js";
-import { Button } from "./ui/button.js";
+import { BusinessPlan } from "./pages/business-plan.js";
+import { ProjectIdea } from "./pages/project-idea.js";
+import { Contact } from "./pages/contact.js";
+import { pageData } from "./page-data.js";
 
-let content = 'asdas dasd asdasd asd asd asda sd';
-let h = new Home('title');
-h.setContent(content);
-h.appendToElement('main');
+class Main {
+    constructor() {
+        let home = new Home(pageData[0].title);
+        window.onload = home.renderPage();
+        home.renderPageByClick('#home');
 
-/* let b = new Button('Hej');
-b.appendToElement('body'); */
+        let bP = new BusinessPlan(pageData[1].title);
+        bP.renderPageByClick('#business-plan');
+
+        let pI = new ProjectIdea(pageData[2].title);
+        pI.renderPageByClick('#project-idea');
+
+        let contact = new Contact(pageData[3].title);
+        contact.renderPageByClick('#contact');
+    }
+}
+
+new Main();
