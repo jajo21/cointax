2f93faa3e258371fdc63124cb92ac29aec295c42
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-04-07 10:12:18 +0200

    Har ändrat innehållet i Affärsplan och projektide och ändra småsaker i stylingen

--- src/pages/business-plan.js
+++ src/pages/business-plan.js
@@ -10,8 +10,18 @@ export class BusinessPlan extends PageBase {
         getContentString() {
             return `
                 <p style="font-style: italic; text-align: center;">${pageData[1].content.idea}</p>
-                <h2 style="text-align: center;">${pageData[1].content.title}</h2>
-                <p>${pageData[1].content.summary}</p>
+                <br>
+                <h3 style="text-align: center;">${pageData[1].content.summaryTitle}</h3>
+                <p>${pageData[1].content.summaryContent}</p>
+                <h3 style="text-align: center;">${pageData[1].content.backgroundTitle}</h3>
+                <p>${pageData[1].content.backgroundContent1}
+                    <a href="${pageData[1].content.backgroundImage}" target="_blank">
+                    <img id="bp-img" src="${pageData[1].content.backgroundImage}" alt="bild på excel-dokument">
+                    </a> <br><br>
+                    ${pageData[1].content.backgroundContent2}
+                </p>
+                <h3 style="text-align: center;">${pageData[1].content.purposeTitle}</h3>
+                <p>${pageData[1].content.purposeContent}</p>
 
             `;
         }
