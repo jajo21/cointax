0b8871466bdf78f24074f65d885b73623482ff23
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-04-20 09:36:15 +0200

    Fixat en navbar som ser okej ut

---
+++ src/app/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { MenuItems } from './MenuItems.js';
+import { Button } from '../Button.js';
+import './Navbar.css';
+
+class Navbar extends React.Component {
+    state = {clicked: false}
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return(
+            <nav className='NavbarItems'>
+                <h1 className='navbar-logo'>Cointax<i className="fa fa-solid fa-coins"></i></h1>
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fa fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <Button>{MenuItems[4].title}</Button>
+            </nav>
+        ) 
+    }
+}
+
+export default Navbar;
