a32a95a2d3889e3559c057a13b45bf389dccd209
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-04-21 15:01:14 +0200

    Ändrade tillbaka transactions filen

--- src/app/components/Routes/transactions.jsx
+++ src/app/components/Routes/transactions.jsx
@@ -5,26 +5,10 @@ import TransactionHistory from '../Transactions/Transaction-history.jsx';
 import TransactionServices from '../../services/transaction-services.js';
 
 export default class Transactions extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            transactions: []
-        }
-    }
-
-    handleState = () => {
-        let tS = new TransactionServices();
-        let currentTransactions = tS.checkLocalStorage();
-        if(currentTransactions === null) currentTransactions = [];
-        this.setState({
-            transactions: currentTransactions
-        })
-    }
-
     render() {
         return (
             <main>
-                <TransactionForm handleState={this.handleState} transactions={this.state.transactions}/>
+                <TransactionForm/>
             </main>
         )
     }
