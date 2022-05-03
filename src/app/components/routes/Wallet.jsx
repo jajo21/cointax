37e4e409c5f2aadcf5e4c8966dcc23a77b103b16
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-03 22:39:16 +0200

    Lägger till funktoinalitet för plånbokssidan och hämtar information från ett api med mockdata som innehåller transactioner

--- src/app/components/routes/Wallet.jsx
+++ src/app/components/routes/Wallet.jsx
@@ -1,18 +1,75 @@
 import React from 'react';
-/* 
----------------------------------------------------
-
-
-DEN HÄR KLASSEN ÄR INTE FÄRDIG, DEN JOBBAS ÄNNU PÅ
-
-
----------------------------------------------------
- */
+import WalletsService from '../../services/wallets-service';
+import AddWalletForm from '../wallets/Add-wallet-form';
+import WalletTransactions from './Wallet-transactions';
 
 export default class Wallet extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            onClickAddWallet: false,
+            onClickCheckTransactions: false,
+            wallets: [],
+            wallet: {}
+        }
+
+        this.walletsService = new WalletsService();
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            wallets: this.walletsService.getWallets()
+        })
+    }
+
+    handleOnClickAddWallet = () => {
+        this.setState({
+            onClickAddWallet: !this.state.onClickAddWallet,
+        });
+    }
+
+    handleSaveWallet = (wallet) => {
+        this.walletsService.saveWallet(wallet);
+
+        this.setState({
+            wallets: this.walletsService.getWallets()
+        })
+    }
+
+    handleOnClickTransactions = (id) => {
+        this.setState({
+            onClickCheckTransactions: !this.state.onClickCheckTransactions,
+            wallet: this.walletsService.getWallet(id),
+        });
+    }
+
+
     render() {
+        const wallets = this.state.wallets;
         return (
+            <>
                 <h2>Plånbok</h2>
+                <button onClick={this.handleOnClickAddWallet}>Lägg till plånbok</button>
+                {this.state.onClickAddWallet &&
+                    <AddWalletForm saveWallet={this.handleSaveWallet}/>
+                }
+
+                {wallets?.map(wallet => {
+                    console.log(wallet);
+                    return(
+                        <div key={wallet.id}>
+                            <h2>{wallet.walletSite}</h2>
+                            <button onClick={() => this.handleOnClickTransactions(wallet.id)}>Kolla hämtade transaktioner</button>
+                        </div>
+                    )
+                })}
+
+                {this.state.onClickCheckTransactions &&
+                    <WalletTransactions wallet={this.state.wallet}/>
+                }
+
+            </>
         )
     }
 } 
---
+++ src/app/components/wallets/Add-wallet-form.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class AddWalletForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            apiKey: 'ABCD-EFGH-IJKL-MNOP',
+            walletSite: 'Mock-Kryptobörs',
+        }
+    }
+    
+    handleClick = () => {
+
+        let newWallet = {
+            walletSite: this.state.walletSite,
+            apiKey: this.state.apiKey
+        }
+
+        this.props.saveWallet(newWallet);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Lägg till plånbok</h1>
+
+                <label htmlFor="walletSite">Välj vilken kryptobörs du vill hämta din historik ifrån<button>Läs mer</button>
+                </label> 
+                <br />
+
+                <select value={this.state.walletSite} onChange={(e) => this.setState({walletSite: e.target.value})}>
+                    <option value="Mock-Kryptobörs">Mock-Kryptobörs</option>
+                    <option value="Binance">Binance</option>
+                    <option value="Coinbase">Coinbase</option>
+                    <option value="Kraken">Kraken</option>
+                </select>
+                <br />
+
+                <label htmlFor="apiKey">Här ska din api-nyckel registreras<button>Läs mer</button>
+                </label> 
+                <br />
+                <input name='apiKey' type="text" value={this.state.apiKey} onChange={(e) => this.setState({apiKey: e.target.value})} />
+                <br />
+
+                <p>Inputfälten är bara fiktiva just nu, inget kommer att registreras. 
+                    Men klicka på knappen för att hämta hem historik från ett mockat api</p>
+
+                <button onClick={this.handleClick}>Lägg till plånbok</button>
+            </div>
+        );
+
+    }
+}
+
+export default AddWalletForm;
