994b0ce8efa0c0e52fb9c173bf8e07208420b605
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-05 11:03:10 +0200

    Uppdaterat Home komponenten lite lätt

--- src/app/components/routes/Home.jsx
+++ src/app/components/routes/Home.jsx
@@ -15,7 +15,12 @@ export default class Home extends React.Component {
             <>
                 <h2>(Hem) Välkommen!</h2>
                 <p>Applikationen är under konstruktion!</p>
-                <p>Menyvalet som har en påbörjad funktionalitet är "Transaktioner" där kan man se historik och lägga till nya transaktioner</p>
+                <p>Menyvalen som har en påbörjad funktionalitet är "Plånbok", "Transaktioner" och "Admin"</p>
+                <ul>
+                    <li>Plånbok: Här ska man kunna koppla sin privata historik från olika kryptobörser. Just nu finns mockad data med funktioner här.</li>
+                    <li>Transaktioner: Här ska man kunna se alla sina transaktioner, även lägga till manuella transaktioner. Just nu finns bara funktionalitet för att se, lägga till och ta bort de manuella transaktionerna.</li>
+                    <li>Admin: Här kan du se alla valutor som kan hanteras av applikationen just nu, du kan även lägga till och ta bort fler valutor.</li>
+                </ul>
             </>
         )
     }
