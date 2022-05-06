912b1d942dc114c70ea6ce7f24af5c7c7eef4090
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-06 17:57:55 +0200

    Lagt till kod för felhantering, ej klar

--- src/app/services/api/coins-api-caller.js
+++ src/app/services/api/coins-api-caller.js
@@ -1,38 +1,56 @@
 class CoinsApiCaller {
     constructor() {
-        this.baseURL = 'https://retoolapi.dev/Z04hfX/';
+        this.baseURL = 'https://retoolapi.dev/sX9GgF/';
     }
 
     getCurrencies = async () => {
         const URL = this.baseURL + 'cointaxcoins';
-        const response = await fetch(URL);
-        const data = await response.json();
-        return data;
+
+        try {
+            const response = await fetch(URL);
+            if(!response.ok) {
+                throw new Error(response.status)
+            }
+            const data = await response.json();
+            return data;
+        } catch(error) {
+            console.log(error);
+        }
 
     }
 
     postCurrency = async (currencyName, currencySymbol, currencyType) => {
         const URL = this.baseURL + 'cointaxcoins';
-        const response = await fetch(URL, {
-            method: 'POST',
-            headers: {'Content-type': 'application/json; charset=UTF-8'},
-            body: JSON.stringify({
-                "name": currencyName,
-                "symbol": currencySymbol,
-                "currencyType": currencyType
-            })
-        });
-        const data = await response.json();
-        return data;
+        try {
+            const response = await fetch(URL, {
+                method: 'POST',
+                headers: {'Content-type': 'application/json; charset=UTF-8'},
+                body: JSON.stringify({
+                    "name": currencyName,
+                    "symbol": currencySymbol,
+                    "currencyType": currencyType
+                })
+            });
+            if(!response.ok) {
+                throw new Error(response.status)
+            }
+        } catch(error) {
+            console.log(error);
+        }
     }
 
     deleteCurrency = async (id) => {
         const URL = this.baseURL + 'cointaxcoins/' + id;
-        const response = await fetch(URL, {
-            method: 'DELETE'
-        });
-        const data = await response.json();
-        console.log('asd', data);
+        try {
+            const response = await fetch(URL, {
+                method: 'DELETE'
+            });
+            if(!response.ok) {
+                throw new Error(response.status)
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
