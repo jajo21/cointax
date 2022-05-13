cd75bdf2d011d6471456619121f21f238ffe96b1
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-13 14:41:19 +0200

    Flyttat api-callers

--- src/app/components/transactions/Transaction-select.jsx
+++ src/app/components/transactions/Transaction-select.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import CoinsApiCaller from '../../services/api/coins-api-caller';
+import CoinsApiCaller from '../../api-callers/coins-api-caller';
 
 class TransactionSelect extends React.Component {
     constructor(props) {
