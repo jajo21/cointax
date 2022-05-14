fc76dd5cbdb93a1320d2ddc7b931e544e6541fc6
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-14 21:45:13 +0200

    Fixat WalletTransactionsRoute, fick bort en fil

--- src/app/components/routes/Wallet-transactions-route.jsx
+++ src/app/components/routes/Wallet-transactions-route.jsx
@@ -1,12 +1,51 @@
-import { useParams } from "react-router-dom";
-import WalletTransactions from "../wallet/Wallet-transactions";
+import React, {useState, useEffect} from "react";
+import { useParams, Link } from "react-router-dom";
+import WalletsService from "../../services/wallets-service";
+import './css/wallet-transactions-route.css'
 
 export function WalletTransactionsRoute() {
 	const params = useParams();
+	const [transactions, setTransactions] = useState(null);
+	const [searchInput, setSearchInput] = useState('');
+	const walletsService = new WalletsService();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			let wallet = await walletsService.getWallet(params.walletSite);
+			let transactions = await walletsService.getWalletTransactions(wallet.apiURL);
+			setTransactions(transactions);
+		}
+		fetchData();
+	}, []);
+
+	const filteredTransactions = walletsService.filterTransactions(transactions || [], searchInput);
 	
 	return (
-		<>
-            <WalletTransactions walletSite={params.walletSite} />
-		</>
+		<div className='wallet-transactions-content'>
+			<h2>Transaktionshistorik från {params.walletSite}</h2>
+
+			<div className='wallet-transactions-buttons'>
+				<button className='connect-button'>Koppla ihop hämtade transaktioner med manuella</button>
+				<Link to="/wallet"><button className='go-back-button'>Gå tillbaka till Plånbok</button></Link>
+			</div>
+
+			<div className='search-bar'>
+				<label htmlFor="search-bar">Sök efter köpta valutor <br />
+					<input name="search-bar" placeholder='EX: BTC' type="text" onChange={(e) => setSearchInput(e.target.value)}/>
+				</label>
+			</div>
+
+			<div className='wallet-transactions'>
+				{filteredTransactions?.map((transaction) => {
+					return (                       
+						<div key={transaction.id} className='wallet-transaction'>
+							<p>{transaction.date}</p>
+							<p>{`Köpt ${transaction.cNameBought}: ${transaction.sumBought}`}</p>
+							<p>{`Sålt ${transaction.cNameSold}: ${transaction.sumSold}`}</p>
+						</div>
+					);
+				})}
+			</div>
+		</div>
 	);
 }
