ca166508c4e2d490ff44ec2e68299802a8399f50
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-18 14:31:38 +0200

    La till en ErrorBoundary, ska bara fixa till den lite mer!

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,19 +5,22 @@ import { Outlet } from 'react-router-dom';
 import Footer from './components/footer/Footer.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
 import { TransactionsProvider } from './contexts/TransactionsContext.js';
+import ErrorBoundary from './components/error/Error-boundary.jsx';
 
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <TransactionsProvider>
-                    <Navbar />
-                    <main>
-                        <Outlet />
-                    </main>
-                    <Footer />
-                </TransactionsProvider>
+                    <TransactionsProvider>
+                        <Navbar />
+                        <main>
+                            <ErrorBoundary>
+                                <Outlet />
+                            </ErrorBoundary>
+                        </main>
+                        <Footer />
+                    </TransactionsProvider>
             </div>
         )
     }
