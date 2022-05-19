7d3e282212fb6dc915453bb4122c680b0fd63023
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-19 11:23:56 +0200

    Testar runt lite med Error-boundarys

---
+++ src/app/components/error/Error-demo.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class ErrorDemo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            count: this.state.count + 1,
+        })
+    }
+
+    render() {
+        if (this.state.count === 5) {
+            throw new Error ('Error: you reached five!');
+        }else {
+            return (
+                <div>
+                    <h3>ErrorBoundary demo</h3>
+                    <p>(försök nå 5)</p>
+                    {this.state.count}
+                    <br />
+                    <button onClick={this.handleClick}>Plus</button>
+                </div>
+            )
+        }
+    }
+}
