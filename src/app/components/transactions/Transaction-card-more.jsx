053d17b3bc4e05660ddd41ba4620d448044bb6eb
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-23 19:28:12 +0200

    Lagt till så användare ser vilken plånbok transaktionen kommer ifrån eller om den är manuell

--- src/app/components/transactions/Transaction-card-more.jsx
+++ src/app/components/transactions/Transaction-card-more.jsx
@@ -8,16 +8,18 @@ class TransactionCardMore extends React.Component {
     render() {
         let date = new Date(this.props.transaction.date);
         let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', secounds: 'numeric'};
+        const transaction = this.props.transaction;
         return(
             <>
                 <div className='transaction-more-info-content'>
                     <p>{date.toLocaleDateString("sv-SV", options)}</p>
-                    <p>{`Transaktion: ${this.props.transaction.id}`}</p>
-                    <p>{`Köpt: ${this.props.transaction.sumBought} ${this.props.transaction.cNameBought}`}</p>
-                    <p>{`Kostnad: ${this.props.transaction.sumSold} ${this.props.transaction.cNameSold}`}</p>
+                    <p>{`Transaktions-ID: ${transaction.id}`}</p>
+                    <p>{`Plånbok/kryptobörs: ${transaction.walletSite}`}</p>
+                    <p>{`Köpt: ${transaction.sumBought} ${transaction.cNameBought}`}</p>
+                    <p>{`Kostnad: ${transaction.sumSold} ${transaction.cNameSold}`}</p>
                 </div>
                 <div>
-                    <button className='delete-button' onClick={() => this.context.deleteTransaction(this.props.transaction.id)}>Ta Bort</button>
+                    <button className='delete-button' onClick={() => this.context.deleteTransaction(transaction.id)}>Ta Bort</button>
                 </div>
             </>
         )
