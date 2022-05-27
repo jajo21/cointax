c1503d9298c181be406f44195d2274fb382ae635
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-05-27 09:39:29 +0200

    Redigerar ej färdiga routes

--- src/app/components/routes/Home.jsx
+++ src/app/components/routes/Home.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Modal from '../modal/Modal'
-import ErrorDemo from '../error/Error-demo';
-import ErrorBoundary from '../error/Error-boundary';
+
 /* 
 ---------------------------------------------------
 
-
 DEN HÄR KLASSEN ÄR INTE FÄRDIG, DEN JOBBAS ÄNNU PÅ
-
 
 ---------------------------------------------------
  */
@@ -18,38 +14,10 @@ export default class Home extends React.Component {
         return (
             <>
                 <h2>(Hem) Välkommen!</h2>
+                <br />
                 <p>Applikationen är under konstruktion!</p>
-                <p>Menyvalen som har en påbörjad funktionalitet är "Plånbok", "Transaktioner" och "Admin"</p>
-                <ul>
-                    <li>Plånbok: Här ska man kunna koppla sin privata historik från olika kryptobörser. Just nu finns mockad data med funktioner här.</li>
-                    <li>Transaktioner: Här ska man kunna se alla sina transaktioner, även lägga till manuella transaktioner. Just nu finns bara funktionalitet för att se, lägga till och ta bort de manuella transaktionerna.</li>
-                    <li>Admin: Här kan du se alla valutor som kan hanteras av applikationen just nu, du kan även lägga till och ta bort fler valutor.</li>
-                </ul>
-
-                {/* Ska tas bort */}
-
-                <button onClick={() => { this.modal.setModal(true) }}>Show Modal</button>
-
-                <Modal
-                    title={'Min titel'}
-                    onMount={(modal) => {
-                        this.modal = modal;
-                    }}
-                >
-
-                    <h3>Hejsan</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Totam tempore quos pariatur aspernatur nam libero aut atque
-                        sapiente explicabo alias, excepturi ad a ullam recusandae sit
-                        qui facilis molestias eveniet.</p>
-                </Modal>
-
-                <br></br><br /><br />
-                <ErrorBoundary>
-                    <ErrorDemo />
-                </ErrorBoundary>
-
-
+                <br /><br />
+                <p>Här på landningssidan kommer det finnas en tydlig överblick vad Cointax är och gör för att förbättra kryptoinvesterares liv.</p>
             </>
         )
     }
